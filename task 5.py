c41786c9ce9ad8640b30b7623dfec465502fce87
Amiramor88 <72005657+Amiramor88@users.noreply.github.com>
2020-11-16 09:55:49 +0200

    Update task 5.py
    working on recommendations

--- task 5.py
+++ task 5.py
@@ -1,11 +1,4 @@
-def AreaTri():
-    a = ''
-    b = ''
-    c = ''
-
-    a = int(input('enter a number:'))
-    b = int(input('enter a number:'))
-    c = int(input('enter a number:'))
+def Area_of_Triangle(a,b,c):
 
     import math
 
@@ -13,4 +6,4 @@ def AreaTri():
     Area = math.sqrt(s*(s-a)*(s-b)*(s-c))
 
     return Area
-print(AreaTri())
+print(Area_of_Triangle(14,11,16))
