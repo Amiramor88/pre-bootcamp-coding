2884c799f145e8d89f2a939ea691b4ad1933c990
Amiramor88 <72005657+Amiramor88@users.noreply.github.com>
2020-11-16 09:57:26 +0200

    Update task 4.py
    working on recommendations

--- task 4.py
+++ task 4.py
@@ -1,19 +1,13 @@
-def check3():
-    firstno = ''
-    secondno = ''
-
-    firstno = input('enter no: ')
-    secondno = input('enter no: ')
-    adding = int(firstno) + int(secondno)
+def check_for_no_3(x,y):
+    
+    add_x_y = int(x) + int(y)
     num = '3'
 
-    value1 = num in firstno
-    value2 = num in secondno
-    value3 = num in str(adding)
+    value3 = num in str(add_x_y)
 
-    if value1==True or value2==True and value3 == True:
+    if (value1 = 3 or value2 =3) and value3:
         return True
     else:
         return False
 
-print(check3())
+print(check3_for_no_3(3,60))
