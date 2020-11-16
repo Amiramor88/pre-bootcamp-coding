282240b10a2b96ce9df17fb34b8bc36d33b6278f
Amiramor88 <72005657+Amiramor88@users.noreply.github.com>
2020-11-16 10:15:21 +0200

    Update task 8.py
    working on recommendations

--- task 8.py
+++ task 8.py
@@ -1,12 +1,17 @@
-def convert():
-    time =""
-
-    time = int(input("how much time in minutes?"))
-
+def convert(time):
+    
+    
     hours = time // 60
 
     minutes = time % 60
+    
+    if hours == 1:
+        return (str(hours) + " hour, " + str(minutes) + " minutes")
+    elif minutes == 1:
+        return (str(hours) + " hours, " + str(minutes) + " minute")
+    elif hours == 1 and minutes == 1:
+        return (str(hours) + " hour, " + str(minutes) + " minute")
+    else:
+        return ( str(hours) + " hours, " + str(minutes) + " minutes")
 
-    return ( str(hours) + " hours, " + str(minutes) + " minutes")
-
-print(convert())
+print(convert(167))
