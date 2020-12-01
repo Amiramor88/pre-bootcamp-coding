0c1283d2b510649385022089f9904f443c2dba9d
Amiramor88 <72005657+Amiramor88@users.noreply.github.com>
2020-12-01 20:17:46 +0200

    Update task 7.py

--- task 7.py
+++ task 7.py
@@ -1,12 +1,9 @@
 def tempc(num):
     
     Celsius = (num - 32) * 5/9
-    print(str(Celsius) + "Degrees Celsius")
+    print(str(Celsius) + " Degrees Celsius")
     
 def tempf(num):
     
     Farenheit = ((num * 9)/5) + 32
-    print(str(Farenheit) + "Degrees Farenheit")
-    
-
-
+    print(str(Farenheit) + " Degrees Farenheit")
